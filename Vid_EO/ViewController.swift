e06bd3561a7478240496d637237603aa1bd79dfc
Ashr005 <as2733@sussex.ac.uk>
2024-04-03 00:21:17 +0100

    Update ViewController.swift

--- Vid_EO/ViewController.swift
+++ Vid_EO/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    private func findvideo(){
+        guard let path = Bundle.main.path(forResource: "test", ofType: "m4v") else {
+            debugPrint("video.m4v not found")
+            return
+        }
+    }
 
 }
 
